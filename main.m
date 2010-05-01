50ad22d8695abc8cb874fdfc9f15271442521ce4
Constantine Fry <constantine.fry@gmail.com>
2010-05-01 14:50:44 +0700

    add project files

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrazyPainter
+//
+//  Created by Fry Constantine on 10/4/09.
+//  Copyright home 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
